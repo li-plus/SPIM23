6e456eb8e8eb522029e5148a5f847a8d6121641e
张翔 <xiang-zh17@mails.tsinghua.edu.cn>
2019-09-27 21:20:55 +0800

    Finish naive ALU

--- thinpad_top.srcs/sources_1/new/alu.v
+++ thinpad_top.srcs/sources_1/new/alu.v
@@ -9,26 +9,25 @@ module ALU(
     );
 
 reg[16:0] f1;
-// sign extension
-wire[4:0] a1 = $signed(a);
-wire[4:0] b1 = $signed(b);
+wire[16:0] a1 = $signed(a);
+wire[16:0] b1 = $signed(b);
 
 always @(*) begin
     case(op)
-        `ALU_ADD: f1 = a1 + b1;
-        `ALU_SUB: f1 = a1 - b1;
-        `ALU_AND: f1 = a & b;
-        `ALU_OR:  f1 = a | b;
-        `ALU_XOR: f1 = a ^ b;
-        `ALU_NOT: f1 = ~a;
-        `ALU_SLL: f1 = a << b;
-        `ALU_SRL: f1 = a >> b;
-        `ALU_SRA: f1 = a >>> b;
-        `ALU_ROL: f1 = (a >> b) | (a << (16 - b));
-        default: f1 = 0;
+        `ALU_ADD: f1 <= a1 + b1;
+        `ALU_SUB: f1 <= a1 - b1;
+        `ALU_AND: f1 <= $signed(a & b);
+        `ALU_OR:  f1 <= $signed(a | b);
+        `ALU_XOR: f1 <= $signed(a ^ b);
+        `ALU_NOT: f1 <= $signed(~a);
+        `ALU_SLL: f1 <= $signed(a << b);
+        `ALU_SRL: f1 <= $signed(a >> b);
+        `ALU_SRA: f1 <= $signed($signed(a) >>> b);
+        `ALU_ROL: f1 <= $signed((a << b) | (a >> (16 - b)));
+        default: f1 <= 0;
     endcase
 end
 
 assign f = f1[15:0];
-assign of = f1[15] ^ f1[16];
+assign of = f1[16] ^ f1[15];
 endmodule
